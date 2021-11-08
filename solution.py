e15baea51461c88785be8430898dbe9daf94750e
tanvimoharir <tanvimoharir@gmail.com>
2021-11-08 21:35:50 +0530

    Revert generator

--- solution.py
+++ solution.py
@@ -1,5 +1,7 @@
 def isAllowed(binary):
-    ''' Gets a number in binary and returns whether number of consecutive 0s exceed or equal 4'''
+    """
+    Gets a number in binary and returns whether number of consecutive 0s exceed or equal 4
+    """
     consecutive_zeroes = 0
     count = 0
     for char in binary:
@@ -11,16 +13,25 @@ def isAllowed(binary):
     return consecutive_zeroes < 4
 
 def getCombinations(n):
-    ''' Gets all n digit binary numbers'''
+    """
+     Gets all n digit binary numbers
+    """
+    record = []
     for i in range(1 << n):
-        yield bin(i).replace('0b', '').rjust(n, '0')
+        record.append(bin(i).replace('0b', '').rjust(n, '0'))
+    return record
 
 def getProbability(n):
-    ''' Returns string representation of fraction of probablity that a student witll miss graduation'''
-    # As per problem we cannot allow any student to miss more than or equal to 4 consecutive days which means that when n is less that 4, there is no way a student would miss his/her graduation
+    """
+    Returns probablity of student missing graduation for given n number of days
+    """
+    # As per problem we cannot allow any student to miss more than or equal to 4 consecutive days 
+    # which means that when n is less that 4, there is no way a student would miss his/her graduation
     # we can represent this using binary representation of number having n digits
-    # for n = 5, we have the following combinations 00000, 00001...11111 which is nothing but all the possible 5 digit binary numbers
-    # when the student is absent at the graduation ceremony itself that means we need to check whether the last digit is 0 which can be obtained by the formula 2 ** (n - 1) for when n < 4
+    # for n = 5, we have the following combinations 00000, 00001...11111 which is basically all the 
+    # possible 5 digit binary numbers
+    # when the student is absent at the graduation ceremony itself that means we need to check 
+    # whether the last digit is 0 (for n < 4 we can use 2 ** (n - 1))
     
     if n == 0:
         return "Invalid input value, must be greater than or equal to 1"
@@ -28,7 +39,8 @@ def getProbability(n):
         return str(1 << (n - 1)) + '/' + str(1 << n)
     leaves = 0
     graduation_day = 0 # count of combinations with last digit 0
-    for record in getCombinations(n):
+    records = getCombinations(n)
+    for record in records:
         if isAllowed(record):
             if record[-1] == '0':
                 graduation_day += 1
@@ -38,11 +50,11 @@ def getProbability(n):
 
 if __name__ == "__main__":
     # tests
-    assert getProbability(5) == '14/29'
-    assert getProbability(10) == '372/773'
+    assert getProbability(5) == "14/29"
+    assert getProbability(10) == "372/773"
     print("Basic tests passed")
-    assert getProbability(1) == '1/2'
-    assert getProbability(2) == '2/4'
-    assert getProbability(3) == '4/8'
+    assert getProbability(1) == "1/2"
+    assert getProbability(2) == "2/4"
+    assert getProbability(3) == "4/8"
     assert getProbability(0) == "Invalid input value, must be greater than or equal to 1"
     print("Additional tests passed")
